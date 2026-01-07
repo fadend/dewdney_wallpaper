bc56f1b6666b481f7d29ecba9284032bd0b80680
David Faden (revfad.com) <dfaden@gmail.com>
2026-01-06 20:26:21 -0800

    Add color with continuous embedding of "evenness"
    Try to capture the concept of "evenness" in a continuous
    way; look at the pair of even numbers less than or equal and
    greater than the magnitude and find the distance from the midpoint
    (the odd number between them). The distance from the midpoint is
    maximized for the even endpoints, matching the "peak" at even
    numbers for the original color scheme, int(m) % 2 == 0.

--- dewdney_wallpaper.js
+++ dewdney_wallpaper.js
@@ -3,9 +3,10 @@ class DewdneyWallpaper {
     this.aInput = container.querySelector("#a");
     this.bInput = container.querySelector("#b");
     this.cInput = container.querySelector("#c");
+    this.useColorInput = container.querySelector("#use-color");
     this.canvas = container.querySelector("#canvas");
     this.context = this.canvas.getContext("2d");
-    const inputs = [this.aInput, this.bInput, this.cInput];
+    const inputs = [this.aInput, this.bInput, this.cInput, this.useColorInput];
     inputs.forEach((input) =>
       input.addEventListener("change", () => this.draw()),
     );
@@ -23,16 +24,28 @@ class DewdneyWallpaper {
     const a = parseFloat(this.aInput.value);
     const b = parseFloat(this.bInput.value);
     const c = parseFloat(this.cInput.value);
+    const useColor = this.useColorInput.checked;
     const width = this.canvas.width;
     const height = this.canvas.height;
     this.context.fillStyle = "black";
     this.context.fillRect(0, 0, width, height);
-    this.context.fillStyle = "white";
     for (let i = 0; i < width; i++) {
       for (let j = 0; j < height; j++) {
         const x = a + (c * i) / width;
         const y = b + (c * j) / height;
-        if (parseInt(x * x + y * y) % 2 == 0) {
+        const m = x * x + y * y;
+        let pixelFill = "black";
+        if (useColor) {
+          const evenBelow = 2 * parseInt(m / 2);
+          const diffFromMiddle = Math.abs(m - evenBelow - 1);
+          pixelFill = `hsl(${parseInt(360 * diffFromMiddle)} 100 50)`;
+        } else {
+          if (parseInt(m) % 2 == 0) {
+            pixelFill = "white";
+          }
+        }
+        if (pixelFill != "black") {
+          this.context.fillStyle = pixelFill;
           this.context.fillRect(i, j, 1, 1);
         }
       }
